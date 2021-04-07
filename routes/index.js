2ecbdaaaa1eec81d35be05d965d4d7b03d9adba9
Brainthoughts <acniedner@gmail.com>
2021-04-07 10:20:53 -0700

    Admin is now toggleable

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const express = require("express"),
-    router = express.Router()
+    router = express.Router(),
+    levels = require("../recources/levels.js")
 
 router.get("/", function (req, res) {
     res.render("index/index", {levels: req.session.levels})
 })
 
 router.get("/admin", function (req, res) {
-    req.session.admin = true;
-    for (let level of req.session.levels) {
-        level.completed = true;
-        level.playable = true;
+    if (!req.session.admin) {
+        req.session.admin = true
+        for (let level of req.session.levels) {
+            level.completed = true;
+            level.playable = true;
+        }
+    } else {
+        req.session.admin = false
+        req.session.levels = levels
     }
     res.redirect("/")
 })
