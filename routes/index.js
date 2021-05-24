a90bd61f4ce693cac2c83d9670f1f080a120dae2
Brainthoughts <acniedner@gmail.com>
2021-05-24 10:52:35 -0700

    Improved timer

--- routes/index.js
+++ routes/index.js
@@ -29,8 +29,24 @@ router.get("/congratulations", function (req, res) {
     if (req.session.levels[req.session.levels.length-1].completed) {
         req.session.challenge = true;
         let startTime = new Date(req.session.startTime)
-        let finishTime = new Date()
-        res.locals.timeTaken = (new Date(finishTime - startTime)); //This breaks if you take longer than an hour; "It's not a bug it's a feature"
+        let finishTime;
+        if (!req.session.finishTime) {
+            finishTime = new Date()
+            req.session.finishTime = finishTime
+        } else {
+            finishTime = new Date(req.session.finishTime)
+        }
+        res.locals.hours = finishTime.getHours() - startTime.getHours();
+        res.locals.minutes = finishTime.getMinutes() - startTime.getMinutes();
+        res.locals.seconds = finishTime.getSeconds() - startTime.getSeconds();
+        if (res.locals.seconds < 0){
+            res.locals.seconds += 60;
+            res.locals.minutes -= 1;
+        }
+        if (res.locals.minutes < 0){
+            res.locals.minutes += 60;
+            res.locals.hours -= 1;
+        }
         res.render("index/congratulations")
     } else {
         res.redirect("/")
