783bcdd30b61ac89dfbe28809d07ff9c22e65c96
Brainthoughts <acniedner@gmail.com>
2021-06-04 20:09:32 -0700

    Improved sudoku

--- routes/sudoku.js
+++ routes/sudoku.js
@@ -7,6 +7,17 @@ const express = require("express"),
 
 let rooms = {}
 let players = {}
+let board = [
+    [0, 1, 6, 2, 7, 3, 9, 4, 5],
+    [5, 2, 4, 6, 1, 9, 8, 3, 7],
+    [7, 9, 3, 4, 5, 8, 6, 1, 2],
+    [3, 8, 9, 5, 2, 6, 4, 7, 1],
+    [1, 6, 5, 8, 4, 7, 2, 9, 3],
+    [4, 7, 2, 3, 9, 1, 5, 8, 6],
+    [2, 4, 1, 7, 8, 5, 3, 6, 9],
+    [9, 3, 8, 1, 6, 2, 7, 5, 4],
+    [6, 5, 7, 9, 3, 4, 1, 2, 8]
+]
 
 router.use(function (req, res, next) {
     res.locals.flash = {success: req.flash("success"), info: req.flash("info"), error: req.flash("error")};
